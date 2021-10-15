7a4f0d24319161b2fb57cfc84058cf617ca94ae0
johnconnor <zkmd2012@inbox.ru>
2021-10-15 09:50:09 +0500

    3 Other mouse events

--- app.js
+++ app.js
@@ -6,12 +6,29 @@ const app = Vue.createApp({
             title: 'The Game of Thrones',
             author: 'John Connor',
             age: 99,
+            layerX: 0,
+            layerY: 0,
+            offsetX: 0,
+            offsetY: 0,
         }
     },
     //any functions of the component we place here
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if(data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            console.log(e)
+            this.layerX = e.layerX
+            this.layerY = e.layerY
+            this.offsetX = e.offsetX
+            this.offsetY = e.offsetY
         }
     },
 })
